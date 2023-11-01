f6f4c97c907f74df08162c2bf0bad7eec39f21f4
Kenny_MacOS_Silicon <124447672+KPerez13@users.noreply.github.com>
2023-11-01 19:54:45 -0400

    first_project

---
+++ block_letters.py
@@ -0,0 +1,15 @@
+# Kenny Perez
+# Peace is a lie, there is only passion
+
+# Should've just done this in the first place smh
+
+full_name = '''
+K       K       P P P P
+K     K         P       p
+K   K           P       p 
+K K             P p p p
+K   K           P
+K     K         P
+K       K       P
+'''
+print(full_name)
