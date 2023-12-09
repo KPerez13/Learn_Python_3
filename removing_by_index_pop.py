ef9ac5d29b1329f35a113495da13eabc8e882c1c
Kenny_MacOS_Silicon <124447672+KPerez13@users.noreply.github.com>
2023-12-08 23:48:49 -0500

    Python_lists_expanded

---
+++ removing_by_index_pop.py
@@ -0,0 +1,15 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+#Task2
+test_variable = data_science_topics.pop()
+print(data_science_topics)
+print(test_variable)
+
+#Task3
+data_science_topics.pop(3)
+print(data_science_topics)
+
+
+
