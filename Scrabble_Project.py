54670c836e52783788c6e10917ed762a3ebfbeca
Kenny-Work <124447672+KPerez13@users.noreply.github.com>
2025-07-16 20:25:31 -0400

    "finishing up project scrabble"

--- Scrabble_Project.py
+++ Scrabble_Project.py
@@ -1,4 +1,3 @@
-# Scrabble_Project
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
@@ -75,6 +74,23 @@ player_to_points = {}
 
 # Within your loop, create a variable called player_points and set it to 0.
 
-for items in player_to_words:
-  player_to_words
+# Task 12
+# Within the loop, create another loop that goes through each word in words and adds the value of score_word() with word as an input.
+
+# Task 13(only move on once you've completed Task 12)
+# After the inner loop ends, set the current player value to be a key of player_to_points, with a value of player_points.
+
+# Task 14(After task 13 was complete)
+# player_to_points should now contain the mapping of players to how many points they’ve scored. Print this out to see the current standings for this game!
+
+# If you’ve calculated correctly, wordNerd should be winning by 1 point.
+
+for player, words in player_to_words.items():
+  player_points = 0
+  for word in words:
+    player_points += score_word(word)
+  player_to_points[player] = player_points
+
+print(player_to_points)
+
 
